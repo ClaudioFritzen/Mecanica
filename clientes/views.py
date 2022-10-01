0904db8e2c9e63358dcc3aea593a5be870ce2a04
ClaudioFritzen <sdfritzen96@gmail.com>
2022-10-01 23:06:12 +0100

    criamos as bases de hmtl, css, e js

--- clientes/views.py
+++ clientes/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse, Http404
 # Create your views here.
 def clientes(request):
-    return HttpResponse('Hello World!')
+    return render(request, 'clientes.html')
