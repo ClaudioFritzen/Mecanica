fbceeb7fd7e9582825032421b16bffa9b1834cb0
ClaudioFritzen <sdfritzen96@gmail.com>
2022-10-10 22:44:09 +0100

    criando o banco de dados sobre serviços prestados

--- servicos/models.py
+++ servicos/models.py
@@ -1,3 +1,16 @@
+from email.policy import default
+from unittest.util import _MAX_LENGTH
 from django.db import models
+from clientes.models import Cliente
 
 # Create your models here.
+class Servico(models.Model):
+    titulo = models.CharField(_MAX_LENGTH=30)
+    cliente = models.ForeignKey(Cliente, on_delete=models.SET_NULL, null = True)
+
+    data_inicio = models.DateField(null=True)
+    date_entrega = models.DateField(null=True)
+    finalizado = models.BooleanField(default=True)
+
+    #protocolo de serviço
+    protocolo = models.CharField(max_length=32, null=True, blank=True)
