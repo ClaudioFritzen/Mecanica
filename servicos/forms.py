200e8a16cbd122edc91a793b3c7c026b4e85af5f
ClaudioFritzen <sdfritzen96@gmail.com>
2022-10-16 00:48:04 +0100

    colocamos todos os campos o bootstrap p todos os fields

--- servicos/forms.py
+++ servicos/forms.py
@@ -4,6 +4,7 @@ from .models import Servico, CategoriaManutencao
 class FormServico(ModelForm):
     class Meta:
         model = Servico
+        """
         # trazendo todos os campos em uma linha
         # fields = '__all__'
 
@@ -15,5 +16,21 @@ class FormServico(ModelForm):
         # contato na hora de criar um novo serviço 
         # o finalizado, quando inicia um serviço ele não tem como ta finalizado
         # e o n° de protocolo que é gerado automaticamente pelo sistema
+        """
         exclude  = ['finalizado', 'protocolo']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        """
+            estilizar o html criado pelo django
+            pegando uma linha e colocando dentro do css
+            self.fields['titulo'].widget.attrs.update({'class': 'form-control'})
+
+            Mas fazer isso pra cada campo seria trabalhoso de mais, e ia ter muito codigo repetido
+            entao faremos um "for" e conseguimos atribuir a classe para todos os campos
+
+        """
+        for fields in self.fields:
+            self.fields[fields].widget.attrs.update({'class': 'form-control'})
+        
 
