38514345f8f6d452200e07ae0829d59fc1fca599
ClaudioFritzen <sdfritzen96@gmail.com>
2022-10-20 01:03:41 +0100

    validamos os formularios

--- servicos/views.py
+++ servicos/views.py
@@ -6,5 +6,15 @@ from .forms import FormServico
 
 # Create your views here.
 def novo_servico(request):
-    form = FormServico()
-    return render(request, 'novo_servico.html', {'form': form})
+    if request.method == "GET":
+        form = FormServico()
+        return render(request, 'novo_servico.html', {'form': form})
+    elif request.method == "POST":
+        form = FormServico(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return HttpResponse('Dados salvo com sucesso')
+
+        else:
+            return render(request, 'novo_servico.html', {'form': form})
