29f5ce43c9cd64126c4d3f89b2c964418017b4f1
ClaudioFritzen <sdfritzen96@gmail.com>
2022-10-20 01:16:31 +0100

    cadastremos mais opções de serviços

--- servicos/choices.py
+++ servicos/choices.py
@@ -4,3 +4,8 @@ class ChoicesCategoriaManuntencao(TextChoices):
     TROCAR_VALVULA_MOTOR = "TVM", "trocar válcula do motor"
     TROCAR_OLEO = "TO", "Troca de óleo"
     BALANCEAMENTO = "B", "Balanceamento"
+    GEOMETRIA = "GEO", "Geometria"
+    TROCA_DISCO_FREIO = "TDF", "Troca de disco de freio"
+    TROCA_VELA = "TV", "Troca de vela"
+    TROCA_FAROLETE ="TF", "Troca do farol frontal"
+    TROCA_SINALEIRA = "TS", "Troca do farol traseiro"
